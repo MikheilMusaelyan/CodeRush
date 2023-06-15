c8144ea9a08bd071cec06b03e017c861ec3e4103
MikheilMusaelyan <mishkamusaeliani@gmail.com>
2023-06-14 23:10:42 -0400

    animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('first', [
+      state('initial', style({
+        transform: 'translate(0, 300px)',
+        opacity: 0
+      })),
+      state('final', style({
+        tansform: 'translate(0, 0)',
+        opacity: 1
+      })),
+      state('final-left', style({
+        transform: 'translate(-70px, 0)',
+        opacity: 0,
+      })),
+      transition('initial => final', animate('700ms cubic-bezier(0.6, 0.18, 0, 0.98)')),
+      transition('final-left <=> final', [
+        style({ opacity: '0.5' }),
+        animate('150ms cubic-bezier(1, 0.29, 0, 0.18)')
+      ])
+    ]),
+    trigger('second', [
+      state('initial', style({
+        transform: 'translateX(300px)',
+        opacity: 0
+      })),
+      state('final', style({
+        tansform: 'translateY(0)',
+        opacity: 1
+      })),
+      transition('initial <=> final', animate('900ms cubic-bezier(1, 0.29, 0, 0.78)'))
+    ])
+  ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit{
+  firstWord: string = 'initial';
+  secondWord: string = 'initial';
+  thirdWord: string = 'initial';
+  @ViewChild('second', {static: false}) second: any;
 
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const string = ', We All Start Somewhere';
+    const indexToAddOn = 12;
+    let currentIndex = 0;
+
+    setTimeout(() => {
+      this.firstWord = 'final';
+      setTimeout(() => {
+        setTimeout(() => {
+          this.firstWord = 'final-left'
+        }, 250);
+        this.secondWord = 'final'
+
+        setTimeout(() => {
+          const interval = setInterval(() => {
+            this.second.nativeElement.innerHTML = this.second.nativeElement.innerHTML.substring(0, indexToAddOn + currentIndex) + string[currentIndex] + '.';
+            currentIndex++;
+            if (currentIndex >= string.length) {
+              clearInterval(interval);
+            }
+          }, 65);
+        }, 800);
+      
+      }, 1200);
+    }, 0);
+  }
 }
